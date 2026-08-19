5755a79dc4797605e5085928c7bb85127eba2fe3
Elohor25 <elohorekurume@gmail.com>
2026-08-19 15:31:23 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_61y6wUcZX', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7m34sfj8lndcncmi9nfk00cl9h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-2' // e.g. us-east-2
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://bhuio1o8p6.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
