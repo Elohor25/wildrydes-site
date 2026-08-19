246008dd1419f8a373caede8e8110ee28270b1e1
Elohor25 <elohorekurume@gmail.com>
2026-08-19 15:10:40 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SDmE42KBA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '78h4jm9gjjhnaknj8qt4bhnsam', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'eu-north-1_61y6wUcZX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7m34sfj8lndcncmi9nfk00cl9h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://bhuio1o8p6.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
